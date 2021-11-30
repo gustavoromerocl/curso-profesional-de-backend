e46fbb7d1335528068863112c5199c708c7bdd5b
romerodevelopments <gandresrp@gmail.com>
2021-11-29 22:57:23 -0300

    http - servir archivos staticos

--- html/server.js
+++ html/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 
 const app = express();
+ 
+//Servir archivos staticos
+app.use('/assets',express.static('assets')); // use inserta un nuevo middleware
 
+//Enviar html
 app.get('/', function(req,res){
     res.sendFile('index.html', {
         root: __dirname
