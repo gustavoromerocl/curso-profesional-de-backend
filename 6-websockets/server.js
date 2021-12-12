6743565cedde5191f3c5b7d666ceed8a065b2004
romerodevelopments <gandresrp@gmail.com>
2021-12-12 19:27:41 -0300

    websockets - control de comunicacion por usuario en websockets

--- 6-websockets/server.js
+++ 6-websockets/server.js
@@ -56,23 +56,46 @@ app.post('/pendientes', function(req, res){
 
 let server = app.listen(3000);
 let io = socketio(server);
+let sockets = {}
 
 
 
 let usersCount = 0;
 
 io.on('connection', function(socket){
+
+    let userId = socket.request._query.loggeduser
+
+    if (userId) sockets[userId] = socket
+
+    //console.log(sockets);
+    //Actualizar usuario en tiempo real
     usersCount++;
 
     io.emit('count_updated', {count: usersCount});
 
+
     socket.on('new_task', function(data){
-        console.log(data);
-        io.emit('new_task', data);
+        //console.log("user ID" + data.userId);
+        if(data.userId){
+            let userSocket = sockets[data.userId];
+            if(!userSocket) return;
+
+            userSocket.emit('new_task', data);
+        }
     });
 
     socket.on('disconnect', function(){
+
+        Object.keys(sockets).forEach(userId => {
+            let s = sockets[userId];
+            if(s.id == socket.id) sockets[userId] = null       
+        });
+
+        //console.log(sockets);
+
         usersCount--;
+        io.emit('count_updated', {count: usersCount});
     })
 })
 
